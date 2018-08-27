8e356806457f436081a2a3567a5c8eccf5c7d4fb
Simon Høgås <simhog@hotmail.no>
2018-08-27 22:09:32 +0200

    Rename factorization script

--- build.bat
+++ build.bat
@@ -3,5 +3,5 @@ for /f %%i in ('dir /b /a:d run_*') do (
     rmdir /s /q %%i 2>NUL
 )
 popd
-python compare.py test_run.txt
+python factorize.py test_run.txt
 REM type ..\..\Data\Tmp\run_metadata_* 2>NUL
