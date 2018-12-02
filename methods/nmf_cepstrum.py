a7904a199ce4364fb704ec02bf526b637487db5c
Simon Høgås <simhog@hotmail.no>
2018-12-02 13:58:26 +0100

    Add more cepstrum versions

--- methods/nmf_cepstrum.py
+++ methods/nmf_cepstrum.py
@@ -1,16 +1,18 @@
 import numpy as np
 from pyxem import ElectronDiffraction
 
-from utils.preprocess import cepstrum
+from utils.preprocess import cepstrum_power
 from utils.decomposition import decompose_nmf
 
 def factorize(diffraction_patterns, parameters):
+    phase_count = parameters['phase_count']
+
     dps = ElectronDiffraction(diffraction_patterns)
-    dps.map(cepstrum, inplace=True, show_progressbar=False)
-    factor_count = 2
-    decompose_nmf(dps, factor_count)
-    factors = dps.get_decomposition_factors().data
-    loadings = dps.get_decomposition_loadings().data
+    dps_cepstrum = dps.map(cepstrum_power, inplace=False, show_progressbar=False)
+    decompose_nmf(dps_cepstrum, phase_count)
+
+    factors = dps_cepstrum.get_decomposition_factors().data
+    loadings = dps_cepstrum.get_decomposition_loadings().data
     # TODO(simonhog): Return factors from highest index with highest loading to get real-space values
     return (factors, loadings), 'decomposition'
 
--- utils/preprocess.py
+++ utils/preprocess.py
@@ -3,10 +3,42 @@ import numpy as np
 def cepstrum(z):
     z = np.fft.fft2(z)
     z = z**2
-    z = np.log(1 + np.abs(z))
+    z = np.log1p(np.abs(z))
     z = np.fft.ifft2(z)
     z = np.fft.fftshift(z)
     z = np.abs(z)
     z = z**2
     return z
 
+
+def cepstrum_power(z):
+    z = np.fft.fft2(z)
+    z = z * np.conj(z)
+    np.seterr(divide='ignore')
+    z = np.log(z)
+    np.seterr(divide='warn')
+    z = np.fft.ifft2(z)
+    z = np.fft.fftshift(z)
+    z = z * np.conj(z)
+    return z.real
+
+
+def cepstrum_complex(z):
+    z = np.fft.fft2(z)
+    np.seterr(divide='ignore')
+    z = np.log(z)
+    np.seterr(divide='warn')
+    z = np.fft.ifft2(z)
+    z = np.fft.fftshift(z)
+    return z
+
+
+def cepstrum_real(z):
+    z = np.fft.fft2(z)
+    z = np.abs(z)
+    np.seterr(divide='ignore')
+    z = np.log(z)
+    np.seterr(divide='warn')
+    z = np.fft.ifft2(z)
+    z = np.fft.fftshift(z)
+    return z
